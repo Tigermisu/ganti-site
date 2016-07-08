77fc452cb30fe22c72d7d69c97b419f5bbde40b5
Chris Jaquez <chris.jaquez@orderos.mx>
2016-07-08 10:11:06 -0600

    Added responsiveness on some broken parts, added mailer script

--- js/app.js
+++ js/app.js
@@ -68,7 +68,68 @@ $(function(){
     });
         
     $(window).resize();
+    
+    setUpForm();
 });
+
+function setUpForm() {
+    // Get the form.
+    var form = $('#contactForm');
+    // Get the messages div.
+    var formMessages = $('#formOutput');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(e) {
+        // Stop the browser from submitting the form.
+        e.preventDefault();
+        $("#contactForm button").text("Enviando...");
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+        // Submit the form using AJAX.
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            })
+            .done(function(response) {
+                // Make sure that the formMessages div has the 'success' class.
+                $(formMessages).removeClass('alert alert-danger');
+                $(formMessages).addClass('alert alert-success');
+
+                // Set the message text.
+                $(formMessages).text(response);
+
+                setTimeout(function() {
+                    $(formMessages).fadeOut();
+                }, 5000);
+
+                // Clear the form.
+                $('#name').val('');
+                $('#empresa').val('');
+                $('#email').val('');
+                $('#info').val('');
+                $("#contactForm button").text("Enviar");
+            })
+            .fail(function(data) {
+                // Make sure that the formMessages div has the 'error' class.
+                $(formMessages).removeClass('alert alert-success');
+                $(formMessages).addClass('alert alert-danger');
+
+                // Set the message text.
+                if (data.responseText !== '') {
+                    $(formMessages).text(data.responseText);
+                    $("#contactForm button").text("Enviar");
+                } else {
+                    $(formMessages).text('Oops! Ocurrió un error: no se pudo enviar la forma.');
+                    $("#contactForm button").text("Enviar");
+                }
+            });
+
+    });
+
+}
 
 function initMap() {
     var mapCanvas = document.getElementById('googlemap');
